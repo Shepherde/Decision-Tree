878e4958c75a46d5da94f1e7f0dfaba2196ae18b
omarsagoo <om.sagoo@gmail.com>
2020-11-21 16:37:05 -0800

    add comments to trewartha function

--- API/trewartha.py
+++ API/trewartha.py
@@ -7,19 +7,25 @@ from OpenWeather import weather_fetch
 URI = "http://www.meteotemplate.com/template/plugins/climateClassification/calcTrewartha.php?hemisphere=N&t1=0&t2=5.4&t3=6.8&t4=12.1&t5=14.2&t6=19.4&t7=21.1&t8=22.2&t9=16.9&t10=11&t11=7.4&t12=5.4&r1=0&r2=27.1&r3=42&r4=20.4&r5=125.7&r6=200.8&r7=89.6&r8=149.6&r9=122.5&r10=115.7&r11=63&r12=undefined&unitsT=C&unitsR=mm"
 
 def trewartha(zipcode):
+    """Gets the trewartha classification from an online calculator"""
+    
     # Get lat lon data from weather_fetch Open Weather API
     weather_data = weather_fetch(zipcode)
     lat = weather_data[3]
     lon = weather_data[4]
 
+    # Check whether or not the location is in the norther or the southern hemisphere
     hemisphere = "N"
     if lat[0] == "-":
         hemisphere = "S"
 
+    # get the monthly temperature data from the appropriate API calls respectively.
     temp_data = climate_fetch_temp(lat, lon)
     percip_data = climate_fetch_perc(lat, lon)
 
+    # URI for the trewartha calculator
     URI = "http://www.meteotemplate.com/template/plugins/climateClassification/calcTrewartha.php"
+    # params for the trewartha calculator
     params = {
         "hemisphere": hemisphere,
         "t1": str(temp_data["data"]["2005-01"]["avg"]),
@@ -49,6 +55,8 @@ def trewartha(zipcode):
         "unitsT": "C",
         "unitsR": "mm",
     }
+
+    # try the request, if it fails raise an error
     try:
         response = requests.get(URI, params=params)
         return response.text.split(" ", 1)[0]
